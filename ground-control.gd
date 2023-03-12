b7b8ef183399e7e86b94782c3fbf8f5d96172ea8
Bjørnar Prytz <bjornarprytz@gmail.com>
2023-03-12 23:14:48 +0100

    trim skiier

--- ground-control.gd
+++ ground-control.gd
@@ -7,19 +7,16 @@ extends CSGCylinder3D
 @onready var t2 = preload("res://assets/tree2.png")
 @onready var t3 = preload("res://assets/tree3.png")
 
-var _trees : Array[PineTree] = []
 const PIXEL_TO_METER_RATIO = 200
 
 func _ready() -> void:
 	for i in 69:
 		spawn_tree()
 
-
 func spawn_tree():
 	var tree = tree_spawner.instantiate() as PineTree
 	
 	add_child(tree)
-	_trees.push_back(tree)
 
 	# get the tree to the surface
 	tree.tree_sprite.position.x = radius
@@ -28,7 +25,3 @@ func spawn_tree():
 	tree.rotation_degrees.y = randf_range(0, 360)
 	tree.tree_sprite.scale = Vector3.ONE * randf_range(0.2, 0.6)
 	tree.tree_sprite.texture = [t1, t2, t3].pick_random()
-	
-	
-	
-	
